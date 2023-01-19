75799db652c63e7af197c51d6d5a4b3fbd1a997e
S.MacLennan <sam@maclennan.dev>
2023-01-20 00:06:23 +1100

    Finished HN Data fetch and box layout

--- app/hn_column.tsx
+++ app/hn_column.tsx
@@ -21,10 +21,19 @@ interface story {
     let display_objects:story[] =[]
     for(const i in stories){
         let story_data = await fetch(`https://hacker-news.firebaseio.com/v0/item/${stories[i]}.json`)
-        display_objects.push(await story_data.json())
+        let story_obj = await story_data.json()
+        let datetime = new Date(story_obj.time*1000)
+        story_obj.time = `${datetime.toLocaleDateString("en-AU")}`      
+        display_objects.push(story_obj) 
         //console.log(display_objects)
     }
     return display_objects
+  }
+  
+  async function Get_NPR_World(){
+    // NPR no longer gives out keys - I found this one on reddit :)
+    var NPR_API_KEY = "MDA0NjI1NjgyMDEyNjM1MDIyNjE5ZDBlMQ010"
+
   }
 
 export default async function Counter() {
@@ -38,15 +47,18 @@ export default async function Counter() {
         >
           <div className="flex justify-between space-x-3">
             <div className="min-w-0 flex-1">
-              <a href="#" className="block focus:outline-none">
+              <a href={message.url} className="block focus:outline-none">
                 <span className="absolute inset-0" aria-hidden="true" />
-                <p className="truncate text-sm font-medium text-gray-900">{message.title}</p>
+                <p className="truncate text-xs font-medium text-gray-900">{message.title}</p>
+                <div className="grid grid-cols-2">
                 <p className="truncate text-sm text-gray-500">{message.by}</p>
+                <time className="flex-shrink-0 justify-self-end whitespace-nowrap text-sm text-gray-500">
+                {message.time}
+                </time>
+                </div>
+                
               </a>
             </div>
-            <time className="flex-shrink-0 whitespace-nowrap text-sm text-gray-500">
-              {message.time}
-            </time>
           </div>
         </li>
       ))}
